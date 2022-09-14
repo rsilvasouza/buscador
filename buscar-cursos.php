71849bd32a2103ae975131ab3c51a56198644426
Rafael da Silva Souza <silvasouza.rafael@eb.mil.br>
2022-09-14 15:56:14 -0300

    inclusão do css selector e listar conteudo da pagina

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,9 +1,21 @@
 <?php
 
+require 'vendor/autoload.php';
+
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client();
 
-$resposta = $client->request(method:'GET', uri:'https://www.alura.com.br/cursos-online-programacao/php',options:[]);
+$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
 
 $html = $resposta->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach ($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
